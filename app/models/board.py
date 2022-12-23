b6e5994c5574fc6965bb8e188f18b565c0cd627f
Jamaica <jamaica@blackeverywhere.com>
2022-12-23 09:24:22 -0500

    Initial setup and skeletal models

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,6 @@
 from app import db
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    card_id = db.Column(db.Integer, db.ForeignKey(
+        'card.card_id'), nullable=True)
