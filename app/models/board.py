feb082df2a7480634e73e4efa8d11e9869bfc823
Jamaica <jamaica@blackeverywhere.com>
2022-12-26 16:17:37 -0500

    Added to card and board models

--- app/models/board.py
+++ app/models/board.py
@@ -2,5 +2,8 @@ from app import db
 
 class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    card_id = db.Column(db.Integer, db.ForeignKey(
-        'card.card_id'), nullable=True)
+    title = db.Column(db.String)
+    owners_name = db.Column(db.String)
+    cards = db.relationship("Card", back_populates="board")
+# One to many relationship between board and card.
+
--- app/models/card.py
+++ app/models/card.py
@@ -2,3 +2,18 @@ from app import db
 
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    hearts = db.Column(db.Integer)
+    message = db.Column(db.String)
+    board_id = db.Column(db.Integer, db.ForeignKey(
+        'board.board_id'), nullable=True)
+    board = db.relationship("Board", back_populates="cards", lazy=True)
+
+def to_dict(self):
+    card_dict = {}
+    card_dict["card_id"] = self.card_id
+    card_dict["hearts"] = self.hearts
+    card_dict["message"] = self.message
+    card_dict["board_id"] = self.board_id
+    
+    return card_dict
+
